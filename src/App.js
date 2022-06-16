03371de7cf2f3700755c1507e77471d1298cee91
Joshua <Joshua.choi2003@gmail.com>
2022-06-15 22:23:13 -0700

    mmm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import {useSelector, useDispatch} from 'react-redux';
+import React from 'react'
 import './App.css';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import { calculateTotals } from './features/cart/cartSlice';
+import { useEffect } from 'react'
+import Modal from './components/Modal';
+import { getCartItems } from './features/cart/cartSlice';
+
 
 function App() {
+  const dispatch = useDispatch();
+  const {cartItems, isLoading} = useSelector((store)=> store.cart)
+  const {isOpen} = useSelector((store) => store.modal)
+ 
+
+  React.useEffect(()=> {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  React.useEffect(()=>{
+    dispatch(getCartItems());
+  }, [])
+
+  const counter = useSelector((state)=>state.counter);
+
+  if(isLoading){
+    return( <div className='loading'>
+        <h1>loading...</h1>
+    </div>)
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
 
